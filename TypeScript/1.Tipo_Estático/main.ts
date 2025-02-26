212335c4c6dc3d7ade9cf7037285dc3ead4d02e7
Vinicius <161797273+ViniciusGGomes@users.noreply.github.com>
2025-02-26 03:06:41 +0000

    adicionando o typeScript

---
+++ TypeScript/1.Tipo_Estático/main.ts
@@ -0,0 +1,19 @@
+/*
+  TypeScript -> verifica o erro em tempo de compilação
+
+  JavaScript -> detecta erros apenas em tempo de execução
+
+*/
+
+const message = "hello typescript"
+
+// message = "oi" //assign -> atribuir
+
+// message()
+
+const users= {
+  name: "Vinícius",
+  city: "Palmas"
+}
+
+// console.log(users.outro)
