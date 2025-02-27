bcc78211e839bbc256843f72ebe2b3a727243df5
Vinicius <161797273+ViniciusGGomes@users.noreply.github.com>
2025-02-27 15:20:23 +0000

    deleted

--- Guanabara/6.Condicionais/ex01.js
+++
@@ -1,9 +0,0 @@
-//Instalamos o Node exec, ao clicar f8 ele abre o Node
-
-var pais = "frança"
-if(pais != "brasil"){
-    console.log("Você é estrangeiro")
-}else(
-    console.log("Você é brasileiro")
-)
-
