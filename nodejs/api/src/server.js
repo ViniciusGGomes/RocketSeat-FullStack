a415260917c9c7e36f48de31120723a4dbb9ebdf
Vinícius Gomes <161797273+ViniciusGGomes@users.noreply.github.com>
2025-03-02 00:33:26 +0000

    criando um servidor com js

---
+++ nodejs/api/src/server.js
@@ -0,0 +1,25 @@
+import http from "node:http"
+
+const server = http.createServer(async(request, response) => {
+  const {method, url} = request
+
+  if(method === "GET" && url === "/products"){
+    return response.end("LISTAR PRODUTOS")
+  }
+
+  if(method === "POST" && url === "/products"){
+    const buffers = []
+
+    for await(const chunk of request){
+      buffers.push(chunk)
+    }
+
+    console.log(Buffer.concat(buffers).toString())
+  }
+
+  return response.writeHead(404).end("Rota não encontrada!")
+})
+
+server.listen(3333)
+
+
