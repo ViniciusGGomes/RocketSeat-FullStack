9a1f9a504c8bf4e0e76bbb8068a4b094654fdfe3
Vinícius Gomes <161797273+ViniciusGGomes@users.noreply.github.com>
2025-03-04 01:45:28 +0000

    api

---
+++ nodejs/api/src/middlewares/jsonBodyHandler.js
@@ -0,0 +1,21 @@
+export async function jsonBodyHandler(request, response) {
+  // Adiciona cada chunk
+  const buffers = []
+
+  // Coleta os chunks de dados da requisição 
+  for await(const chunk of request){
+    buffers.push(chunk)
+  }
+
+  try {
+    // Concatena os chunk e converte para String. Em seguida, converte a string para JSON.
+    // Middler consegue mudar a requisição!
+    request.body = JSON.parse(Buffer.concat(buffers).toString())
+  } catch (error) {
+    request.body = null
+  }
+
+  // Middler consegue modificar a resposta!
+  response.setHeader("Content-type", "application/json")
+  // Padrão para disser que o tipo do conteúdo é uma application/json, o corpo da requisição 
+}
