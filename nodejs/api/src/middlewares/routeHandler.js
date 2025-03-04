9029c9c79762f25fa7446a3e5db297f93f46ecbc
Vinícius Gomes <161797273+ViniciusGGomes@users.noreply.github.com>
2025-03-04 01:44:50 +0000

    api

---
+++ nodejs/api/src/middlewares/routeHandler.js
@@ -0,0 +1,19 @@
+import { routes } from "../routes.js";
+
+export function routeHandler(request, response){
+  const route = routes.find((route) => {
+    return route.method === request.method && route.path.test(request.url)
+  })
+
+  if(route){
+    const routeParams = request.url.match(route.path)
+    const { ...params } = routeParams.groups
+    
+    request.params = params
+
+    console.log(params)
+
+    return route.Controller(request, response)
+  }
+  return response.writeHead(404).end("Rota não encontrada!")
+}
