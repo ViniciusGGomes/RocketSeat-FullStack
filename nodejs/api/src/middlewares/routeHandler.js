fa98c99d5b2fad51c17994718f82e3d8aae1d71f
Vinícius Gomes <161797273+ViniciusGGomes@users.noreply.github.com>
2025-03-12 21:06:40 +0000

    atualizei

--- nodejs/api/src/middlewares/routeHandler.js
+++ nodejs/api/src/middlewares/routeHandler.js
@@ -1,26 +1,22 @@
 import { routes } from "../routes.js";
 import { extractQueryParams } from "../utils/extract-query-params.js";
-import { Database} from "../database.js"
+import { Database } from "../database.js";
 
-const database = new Database()
+const database = new Database();
 
-
-export function routeHandler(request, response){
+export function routeHandler(request, response) {
   const route = routes.find((route) => {
-    return route.method === request.method && route.path.test(request.url)
+    return route.method === request.method && route.path.test(request.url);
+  });
 
-    
-  })
+  if (route) {
+    const routeParams = request.url.match(route.path);
+    const { query, ...params } = routeParams.groups;
 
-  if(route){
-    const routeParams = request.url.match(route.path)
-    const {query, ...params } = routeParams.groups
+    request.params = params;
+    request.query = query ? extractQueryParams(query) : {};
 
-    
-    request.params = params
-    request.query = query ? extractQueryParams(query) : {}
-
-    return route.Controller({request, response, database}) // Com chaves podemos passar em qualquer sequencia 
+    return route.Controller({ request, response, database }); // Com chaves podemos passar em qualquer sequencia
   }
-  return response.writeHead(404).end("Rota não encontrada!")
+  return response.writeHead(404).end("Rota não encontrada!");
 }
